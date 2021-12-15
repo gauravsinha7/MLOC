edec113c6036f9f6a93cb7e7a74958af9fdf9e1d
Gaurav <gauravsinha952@gmail.com>
2021-12-15 17:13:30 -0500

    Mistake in code of executor which got initially in cascadepredict.py

--- src/runtime/cascadepredict.py
+++ src/runtime/cascadepredict.py
@@ -1,83 +1,128 @@
 import ast
 import copy
-import importlib
 import inspect
-from typing import Callable, MutableMapping, Mapping
+from typing import Mapping, Callable
 
-from src.runtime.graph_builder import GraphBuilder
-from src.runtime.timer import Timer
+import numpy as np
+import pandas as pd
+import scipy
+
+from src.graph.utility import create_syntax_tree, define_syntax_tree
 from src.graph.node import GraphNode
-from src.runtime.cascade_build import construct_cascades
-from src.runtime.cascadepredict import predict_cascades
-
-timing_map_set: MutableMapping[str, MutableMapping[str, float]] = {}
-model_data_set: MutableMapping[str, MutableMapping[str, object]] = {}
-saomls_final_func_set: MutableMapping[str, Callable] = {}
 
 
-def instrument_function(func: Callable, timing_map: MutableMapping[str, float],
-                        model_data: MutableMapping[str, object]) -> Callable:
-    python_source = inspect.getsource(func)
-    python_ast: ast.AST = ast.parse(python_source)
-    function_name: str = python_ast.body[0].name
-    type_discover: Timer = Timer()
-    # Create an instrumented AST that will time all operators in the function.
-    new_ast: ast.AST = type_discover.visit(python_ast)
-    new_ast = ast.fix_missing_locations(new_ast)
+def predict_cascades(func: Callable, model_node: GraphNode,
+                     predict_function: Callable, predict_proba_function: Callable,
+                     cascades_dict: Mapping) -> Callable:
+    class CascadePredict(ast.NodeTransformer):
+        def visit_FunctionDef(self, orig_ast: ast.FunctionDef) -> ast.AST:
+            cascades_body = []
+            # Compute selected features.
+            for node in selected_feature_nodes:
+                cascades_body += node.get_syntax_tree()
+            # Predict with approximate model.
+            approximate_model_ast = define_syntax_tree(fn_name=predict_proba_function.__name__,
+                                                     out_name="__saomls_approximate_preds",
+                                                     in_names=selected_feature_names,
+                                                     model_param="__saomls_approximate_model")
+            cascades_body += approximate_model_ast
+            # Get indices that can't be approximated.
+            unapproximated_indices_ast = create_syntax_tree(fn_name="__saomls_get_unapproximated_indices",
+                                                             out_name="__saomls_unapproximated_indices",
+                                                             in_names=["__saomls_approximate_preds",
+                                                                          "__saomls_cascade_threshold"])
+            cascades_body += unapproximated_indices_ast
+            # Only compute remaining features for unapproximated indices.
+            shortened_inputs = set()
+            for node in remaining_feature_nodes:
+                for input_name in node.in_names:
+                    if input_name not in shortened_inputs:
+                        shorten_ast = create_syntax_tree(fn_name="__saomls_select_unapproximated_rows",
+                                                          out_name=input_name,
+                                                          in_names=[input_name, "__saomls_unapproximated_indices"])
+                        cascades_body += shorten_ast
+                        shortened_inputs.add(input_name)
+                cascades_body += node.get_syntax_tree()
+            # Shorten the selected features.
+            for name in selected_feature_names:
+                shorten_ast = create_syntax_tree(fn_name="__saomls_select_unapproximated_rows",
+                                                  out_name=name,
+                                                  in_names=[name, "__saomls_unapproximated_indices"])
+                cascades_body += shorten_ast
+            # Predict with full model.
+            full_model_ast = define_syntax_tree(fn_name=predict_function.__name__,
+                                              out_name="__saomls_full_preds",
+                                              in_names=model_node.in_names,
+                                              model_param="__saomls_full_model")
+            cascades_body += full_model_ast
+            # Return combined predictions.
+            combine_predictions_ast = create_syntax_tree(fn_name="__saomls_combine_predictions",
+                                                          out_name="__saomls_final_predictions",
+                                                          in_names=["__saomls_approximate_preds",
+                                                                       "__saomls_full_preds",
+                                                                       "__saomls_cascade_threshold"])
+            cascades_body += combine_predictions_ast
+            return_ast = ast.parse("return __saomls_final_predictions", "exec").body
+            cascades_body += return_ast
+            # Finalize AST.
+            new_ast = copy.deepcopy(orig_ast)
+            new_ast.body = cascades_body
+            # No recursion allowed!
+            new_ast.decorator_list = []
+            return ast.copy_location(new_ast, orig_ast)
+
+    func_source = inspect.getsource(func)
+    func_ast = ast.parse(func_source)
+    selected_feature_indices = cascades_dict["selected_feature_indices"]
+    selected_feature_nodes = [model_node.input_nodes[i] for i in selected_feature_indices]
+    selected_feature_names = [model_node.in_names[i] for i in selected_feature_indices]
+    remaining_feature_nodes = [model_node.input_nodes[i] for i in range(len(model_node.input_nodes)) if
+                               i not in selected_feature_indices]
+    cascades_transformer = CascadePredict()
+    cascades_ast = cascades_transformer.visit(func_ast)
+    cascades_ast = ast.fix_missing_locations(cascades_ast)
+    # import astor
+    # print(astor.to_source(cascades_ast))
+    # Create namespaces the instrumented function can run in containing both its
+    # original globals and the ones the instrumentation needs.
     local_namespace = {}
     augmented_globals = copy.copy(func.__globals__)
-    augmented_globals["saomls_timing_map"] = timing_map
-    augmented_globals["saomls_model_data"] = model_data
-    augmented_globals["time"] = importlib.import_module("time")
+    augmented_globals["__saomls_approximate_model"] = cascades_dict["approximate_model"]
+    augmented_globals["__saomls_full_model"] = cascades_dict["full_model"]
+    augmented_globals["__saomls_cascade_threshold"] = cascades_dict["cascade_threshold"]
+    augmented_globals["__saomls_get_unapproximated_indices"] = get_unapproximated_indices
+    augmented_globals["__saomls_select_unapproximated_rows"] = select_unapproximated_rows
+    augmented_globals["__saomls_combine_predictions"] = combine_predictions
     # Run the instrumented function.
-    exec(compile(new_ast, filename="<ast>", mode="exec"), augmented_globals,
+    exec(compile(cascades_ast, filename="<ast>", mode="exec"), augmented_globals,
          local_namespace)
-    return local_namespace[function_name]
+    return local_namespace[func.__name__]
 
 
-def execute(train_function: Callable = None, predict_function: Callable = None,
-                    confidence_function: Callable = None, score_function: Callable = None,
-                    train_cascades_params: MutableMapping = None,
-                    predict_cascades_params: Mapping = None) -> Callable:
-    def execute_inner(func: Callable) -> Callable:
-        func_id: str = "func_id%s" % func.__name__
+def get_unapproximated_indices(approximated_preds, cascade_threshold):
+    return np.logical_and(approximated_preds < cascade_threshold,
+                          approximated_preds > 1 - cascade_threshold).nonzero()[0]
 
-        def function_wrapper(*args):
-            if func_id not in timing_map_set:
-                timing_map_set[func_id] = {}
-                model_data_set[func_id] = {}
-                instrumented_func: Callable = \
-                    instrument_function(func, timing_map_set[func_id], model_data_set[func_id])
-                return instrumented_func(*args)
-            elif func_id not in saomls_final_func_set:
-                timing_map = timing_map_set[func_id]
-                model_data = model_data_set[func_id]
-                function_source = inspect.getsource(func)
-                function_ast = ast.parse(function_source)
-                graph_builder = GraphBuilder(timing_map)
-                graph_builder.visit(function_ast)
-                model_node: GraphNode = graph_builder.get_model_node()
-                if train_cascades_params is not None:
-                    construct_cascades(model_data,
-                                       model_node,
-                                       train_function, predict_function,
-                                       confidence_function, score_function,
-                                       train_cascades_params)
-                    return train_cascades_params["full_model"]
-                elif predict_cascades_params is not None:
-                    cascades_func = predict_cascades(func,
-                                                     model_node,
-                                                     predict_function,
-                                                     confidence_function,
-                                                     predict_cascades_params)
-                    saomls_final_func_set[func_id] = cascades_func
-                    return cascades_func(*args)
-                else:
-                    saomls_final_func_set[func_id] = func
-                    return func(*args)
-            else:
-                return saomls_final_func_set[func_id](*args)
 
-        return function_wrapper
+def select_unapproximated_rows(input_data, unapproximated_indices):
+    if isinstance(input_data, scipy.sparse.csr.csr_matrix) or isinstance(input_data, np.ndarray):
+        return input_data[unapproximated_indices]
+    elif isinstance(input_data, pd.DataFrame):
+        return input_data.iloc[unapproximated_indices].reset_index().drop("index", axis=1)
+    else:
+        return input_data
 
-    return execute_inner
+
+def combine_predictions(approximate_predictions, full_predictions, cascade_threshold):
+    final_predictions = np.zeros(approximate_predictions.shape, dtype=full_predictions.dtype)
+    full_prediction_index = 0
+    for i in range(len(final_predictions)):
+        if approximate_predictions[i] >= cascade_threshold:
+            final_predictions[i] = 1
+        elif approximate_predictions[i] <= 1 - cascade_threshold:
+            final_predictions[i] = 0
+        else:
+            final_predictions[i] = full_predictions[full_prediction_index]
+            full_prediction_index += 1
+    assert(full_prediction_index == len(full_predictions))
+    return final_predictions
